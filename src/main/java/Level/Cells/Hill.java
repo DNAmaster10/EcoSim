6fac633e6f0a7f1badb903b57f1393fbee162357
DNAmaster10 <louishollobon@outlook.com>
2023-02-20 22:09:23 +0000

    Content update

---
+++ src/main/java/Level/Cells/Hill.java
@@ -0,0 +1,12 @@
+package Level.Cells;
+
+import com.raylib.Jaylib;
+import com.raylib.Raylib;
+
+public class Hill {
+    public static class properties {
+        public static Jaylib.Color color = new Jaylib.Color(39, 39, 41, 255);
+        public static Raylib.Texture texture;
+        public static Raylib.Rectangle textureRectangle = new Jaylib.Rectangle(0, 0, 10, 10);
+    }
+}
